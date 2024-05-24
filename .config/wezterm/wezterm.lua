d69478252ab05eacf6446c0feb7485c67c2285a4
Sharquille Andrew <Dequan6413@gmail.com>
2024-05-24 19:08:34 -0400

    wezterm config included

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+-- wezterm api
+local wezterm = require("wezterm")
+
+return {
+	-- color_scheme
+	color_scheme = "Catppuccin Mocha",
+	enable_tab_bar = false,
+	font = wezterm.font("BlexMono Nerd Font Mono", {
+		weight = "Bold",
+		italic = true,
+	}),
+	font_size = 22.6,
+	macos_window_background_blur = 30,
+	window_background_opacity = 0.8,
+	window_decorations = "RESIZE",
+}
